9fb341f328f41d1a2d1031274c5653f03bb73c95
JakobKjellberg02 <jakob.zornigkjellberg@gmail.com>
2025-10-10 14:39:52 +0200

    scuffed code

---
+++ fail2ban/discord-ban-message.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+IP="$1"
+WEBHOOK="$2"
+JAIL="$3"
+
+COUNTRY=$(geoiplookup "$IP" | awk -F: '{gsub(/,/, " "); print $2}' | xargs)
+
+curl -s -X POST -H "Content-Type: application/json" \
+-d "{\"content\":\"**[Fail2Ban]** Banned IP: $IP\nJail: <name>\nCountry: $COUNTRY\nTime: $(date +'%Y-%m-%d %H:%M:%S')\"}" \
+"$WEBHOOK"
