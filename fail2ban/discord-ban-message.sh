76f5b5331fa7147cd5d3d7bd2bfe1e288240f84a
JakobKjellberg02 <jakob.zornigkjellberg@gmail.com>
2025-10-10 14:43:55 +0200

    fixed

--- fail2ban/discord-ban-message.sh
+++ fail2ban/discord-ban-message.sh
@@ -6,5 +6,5 @@ JAIL="$3"
 COUNTRY=$(geoiplookup "$IP" | awk -F: '{gsub(/,/, " "); print $2}' | xargs)
 
 curl -s -X POST -H "Content-Type: application/json" \
--d "{\"content\":\"**[Fail2Ban]** Banned IP: $IP\nJail: <name>\nCountry: $COUNTRY\nTime: $(date +'%Y-%m-%d %H:%M:%S')\"}" \
+-d "{\"content\":\"**[Fail2Ban]** Banned IP: $IP\nJail: $JAIL\nCountry: $COUNTRY\nTime: $(date +'%Y-%m-%d %H:%M:%S')\"}" \
 "$WEBHOOK"
